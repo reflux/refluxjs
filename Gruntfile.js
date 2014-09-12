d2de79626b21bb3afaa430d60d159494c48ca77c
Ben Ripkens <bripkens.dev@gmail.com>
2014-09-12 21:59:04 +0200

    Have a working SauceLabs configuration in the repository
    It should be possible to locally execute tests against SauceLabs.
    Add a second karma Grunt goal through which this can be achieved.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,31 @@
 module.exports = function(grunt) {
 
+  var sauceLaunchers = {
+    sl_chrome: {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'Windows 7',
+      version: '35'
+    },
+    sl_firefox: {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      version: '30'
+    },
+    sl_ios_safari: {
+      base: 'SauceLabs',
+      browserName: 'iphone',
+      platform: 'OS X 10.9',
+      version: '7.1'
+    },
+    sl_ie_11: {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 8.1',
+      version: '11'
+    }
+  };
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
@@ -35,10 +61,21 @@ module.exports = function(grunt) {
       tasks: ['build']
     },
     karma: {
-      integration: {
+      local: {
         configFile: 'karma.conf.js',
         options: {
-            browsers: ['PhantomJS']
+          browsers: ['PhantomJS']
+        }
+      },
+      sauce: {
+        configFile: 'karma.conf.js',
+        options: {
+          reporters: ['saucelabs', 'spec'],
+          sauceLabs: {
+            public: 'public',
+          },
+          customLaunchers: sauceLaunchers,
+          browsers: Object.keys(sauceLaunchers)
         }
       }
     }
@@ -46,7 +83,9 @@ module.exports = function(grunt) {
 
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-  grunt.registerTask('test', ['jshint', 'mochaTest', 'karma']);
+  grunt.registerTask('test', ['jshint', 'mochaTest', 'karma:local']);
+
+  grunt.registerTask('travis', ['test', 'karma:sauce']);
 
   grunt.registerTask('build', ['test', 'browserify', 'uglify']);
 
