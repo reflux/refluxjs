60985f89120ea20b29a3a55d4e855440daebc7f7
KungD <david@krawaller.se>
2014-09-27 16:49:52 +0200

    extract aggregate functionality

--- src/all.js
+++ src/all.js
@@ -1,7 +1,5 @@
 var createStore = require('./createStore');
 
-var slice = Array.prototype.slice;
-
 /**
  * Track a set of Actions and Stores. Use Reflux.all if you need to handle
  * data coming in parallel.
@@ -13,60 +11,10 @@ var slice = Array.prototype.slice;
  *  least once.
  */
 module.exports = function(/* listenables... */) {
-    var numberOfListenables = arguments.length,
-        // create a new array of the expected size. The initial
-        // values will be falsy, which is fine for us.
-        // Once each item in the array is truthy, the callback can be called
-        listenablesEmitted,
-        // these arguments will be used to *apply* the action.
-        args,
-        // the original listenables
-        listenables = slice.call(arguments);
-        // this store combines all the listenables
-        store = createStore({
-            init: function(){
-                for (var i = 0; i < numberOfListenables; i++) {
-                    this.listenTo(listenables[i],newListener(i));
-                }
-                reset();
-            }
-        });
-
-    return store;
-
-    function reset() {
-        listenablesEmitted = new Array(numberOfListenables);
-        args = new Array(numberOfListenables);
-    }
-
-    function newListener(i) {
-        return function() {
-            listenablesEmitted[i] = true;
-            // Reflux users should not need to care about Array and arguments
-            // differences. This makes sure that they get the expected Array
-            // interface
-            args[i] = slice.call(arguments);
-            emitWhenAllListenablesEmitted();
-        };
-    }
-
-    function emitWhenAllListenablesEmitted() {
-        if (didAllListenablesEmit()) {
-            store.trigger.apply(store,args);
-            reset();
+    var listenables = Array.prototype.slice.call(arguments);
+    return createStore({
+        init: function(){
+            this.listenToAggregate.apply(this,listenables.concat("trigger"));
         }
-    }
-
-    function didAllListenablesEmit() {
-        // reduce cannot be used because it only iterates over *present*
-        // elements in the array. Initially the Array doesn't contain
-        // elements. For this reason the usage of reduce would always indicate
-        // that all listenables emitted.
-        for (var i = 0; i < numberOfListenables; i++) {
-            if (!listenablesEmitted[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
+    });
 };
