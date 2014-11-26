272d543e6503cc514875e612fec313b0dfdde3d5
KungD <david@krawaller.se>
2014-11-26 14:51:38 +0100

    removed warning on mixin combo

--- src/connect.js
+++ src/connect.js
@@ -13,19 +13,7 @@ module.exports = function(listenable,key){
             }
         },
         componentDidMount: function(){
-            var warned = false;
-            for(var m in Reflux.ListenerMethods){
-                if (this[m] && typeof console && typeof console.warn === "function" && !warned ){
-                    console.warn(
-                        "Component using Reflux.connect already had property '"+m+"'. "+
-                        "Either you had your own property with that name which was now overridden, "+
-                        "or you combined connect with ListenerMixin which is unnecessary as connect "+
-                        "will include the ListenerMixin methods automatically."
-                    );
-                    warned = true;
-                }
-                this[m] = Reflux.ListenerMethods[m];
-            }
+            _.extend(this,Reflux.ListenerMethods);
             var me = this, cb = (key === undefined ? this.setState : function(v){me.setState(_.object([key],[v]));});
             this.listenTo(listenable,cb);
         },
