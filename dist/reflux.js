e998d4a2fbe327a8b12d5673aadb10d892fd47d9
Mikael Brassman <mishakun@gmail.com>
2014-12-07 11:27:00 +0100

    Updated dist for bower

--- dist/reflux.js
+++ dist/reflux.js
@@ -401,7 +401,7 @@ module.exports = {
      * @param {Function|String} callback The method to call when all publishers have emitted
      * @returns {Object} A subscription obj where `stop` is an unsub function and `listenable` is an array of listenables
      */
-    joinStrict: maker("strict"),
+    joinStrict: maker("strict")
 };
 
 },{"./joins":13,"./utils":16}],5:[function(_dereq_,module,exports){
@@ -529,19 +529,7 @@ module.exports = function(listenable,key){
             }
         },
         componentDidMount: function(){
-            var warned = false;
-            for(var m in Reflux.ListenerMethods){
-                if (this[m] && typeof console && typeof console.warn === "function" && !warned ){
-                    console.warn(
-                        "Component using Reflux.connect already had property '"+m+"'. "+
-                        "Either you had your own property with that name which was now overridden, "+
-                        "or you combined connect with ListenerMixin which is unnecessary as connect "+
-                        "will include the ListenerMixin methods automatically."
-                    );
-                    warned = true;
-                }
-                this[m] = Reflux.ListenerMethods[m];
-            }
+            _.extend(this,Reflux.ListenerMethods);
             var me = this, cb = (key === undefined ? this.setState : function(v){me.setState(_.object([key],[v]));});
             this.listenTo(listenable,cb);
         },
