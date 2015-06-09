55bdc19e4ef766eb204342a523a1cd8a0ac111c0
Stephen Wan <stephen@stephenwan.net>
2015-06-09 00:29:56 -0700

    Add setState and replaceState to StoreMethods, hookup getInitialState

--- src/StoreMethods.js
+++ src/StoreMethods.js
@@ -1,6 +1,23 @@
+var Immutable = require('immutable');
+
 /**
  * A module of methods that you want to include in all stores.
  * This module is consumed by `createStore`.
  */
 module.exports = {
+    init: function() {
+        var initialState = null;
+        if (this.getInitialState) {
+            initialState = this.getInitialState();
+        }
+        this.state = Immutable.Map(initialState);
+    },
+    setState: function(nextState) {
+        this.state = this.state.merge(nextState);
+        this.trigger(this.state.toJS());
+    },
+    replaceState: function(newState) {
+        this.state = Immutable.Map(newState);
+        this.trigger(this.state.toJS());
+    }
 };
